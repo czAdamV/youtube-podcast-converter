bedc190a84398c62d44df3f2e70b6704f2a82c1e
Adam Volek <volek@adamv.cz>
2020-02-06 21:13:25 +0100

    ytproxy tests overhaul
    ytproxy tests now rely on the pytube module mocking. This makes testing
    the module a bit easier and more robust, as future internal pytube
    changes cannot break current tests.

--- tests/test_ytproxy.py
+++ tests/test_ytproxy.py
@@ -1,11 +1,64 @@
 from ytproxy import create_app
+from base64 import a85decode as decode
+import io
 import pathlib
 import pytest
+import pytube
+import urllib
+
+# A minimalistic valid WAV file. Used by YouTubeMock to moct output of valid
+# audio stream. It would be nice if a slnt RIFF chunk could be used to make
+# the audio a bit longer but it seems ffmpeg doesn't support that :(
+audio = decode(";Fs>I-3+#G=$]kUAo2W4&-)\\1!<<-#7'$@g7'$@g!<<B*A79Rg!<<*\"J,f")
+
+
+class PyTubeMock:
+    def __init__(self):
+        self.extract = ExtractMock
+        self.YouTube = YouTubeMock
+
+
+class ExtractMock:
+    def watch_url(url):
+        return url
+
+
+class YouTubeMock:
+    def __init__(self, uri):
+        if 'invalid' in uri:
+            raise pytube.exceptions.RegexMatchError('', '')
+
+        if 'nonexistent' in uri:
+            raise pytube.exceptions.VideoUnavailable('')
+
+        if 'livestream' in uri:
+            raise pytube.exceptions.LiveStreamError()
+
+        if 'generic_error' in uri:
+            raise pytube.exceptions.PytubeError()
+
+        if 'connection_error' in uri:
+            raise urllib.error.URLError('test')
+
+        self.streams = YouTubeMockStreams()
+
+
+class YouTubeMockStreams:
+    def get_audio_only(self):
+        return YouTubeMockAudioOnly()
+
+
+class YouTubeMockAudioOnly:
+    def __init__(self):
+        self.mime_type = 'audio/wav'
+
+    def stream_to_buffer(self):
+        return io.BytesIO(audio)
 
 
 @pytest.fixture
 def app():
-    app = create_app()
+    app = create_app(PyTubeMock())
     app.config['TESTING'] = True
     return app
 
@@ -19,31 +72,18 @@ class TestAPI:
         assert 'allowed_formats' in data
         assert data['allowed_formats'] == []
 
-    @pytest.mark.block_network
-    def test_invalid_video_id(self, app):
-        request = app.test_client().get('/download/a')
+    @pytest.mark.parametrize("uri,code,body", [
+        ('invalid', 400, b'Invalid video_id'),
+        ('nonexistent', 404, b'Video does not exist'),
+        ('livestream', 400, b'Video is a live stream'),
+        ('generic_error', 500, b'Couldn\'t process request'),
+        ('connection_error', 502, b'Bad gateway')])
+    def test_errors(self, app, uri, code, body):
+        request = app.test_client().get(f'/download/{uri}')
 
-        assert request.status_code == 400
-        assert b'Invalid video_id' in request.data
+        assert request.status_code == code
+        assert body in request.data
 
-    @pytest.mark.vcr
-    def test_nonexistent_video(self, app):
-        request = app.test_client().get('/download/12345678912')
-
-        assert request.status_code == 404
-        assert b'Video does not exist' in request.data
-
-    @pytest.mark.vcr
-    @pytest.mark.skip(reason='PyTube throws a KeyError instead of a '
-                      'LiveStreamError when asked to download live stream. '
-                      'This seems to be a bug in PyTube.')
-    def test_is_livestream(self, app):
-        request = app.test_client().get('/download/hHW1oY26kxQ')
-
-        assert request.status_code == 400
-        assert b'Video is a live stream' in request.data
-
-    @pytest.mark.vcr
     def test_download(self, app):
         request = app.test_client().get('/download/7spQqq1o36c')
 
